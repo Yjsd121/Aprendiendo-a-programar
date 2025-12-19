18f900c4dca08dfa0a9f31c8b928f8abeadb4578
Yjsd121 <yadetjsd@gmail.com>
2025-12-18 21:34:02 -0600

    New Project Movie Search

--- Movie_Search/src/App.jsx
+++ Movie_Search/src/App.jsx
@@ -1,5 +1,22 @@
+import './app.css'
+
 export function App () {
   return (
-    <h1>Hello World </h1>
+    <div className='main'>
+      <header>
+        <label>Search movies</label>
+        <form className='search' method='get'>
+          <input placeholder='How to take your Dragon...' />
+          <button type='submit'>
+            Search
+          </button>
+        </form>
+      </header>
+
+      <main>
+        Aqui se vera el resultado de la busqueda
+      </main>
+    </div>
+
   )
 }
